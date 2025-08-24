c8f4ba36942e5c4cf723df6fe18506e3331cee38
share121 <2854631158@qq.com>
2025-08-24 18:24:55 +0800

    feat: 添加 cli 插件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,7 +21,7 @@ mod updater;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let mut builder = tauri::Builder::default();
+    let mut builder = tauri::Builder::default().plugin(tauri_plugin_cli::init());
     #[cfg(desktop)]
     {
         builder = builder.plugin(tauri_plugin_single_instance::init(|app, argv, cmd| {
